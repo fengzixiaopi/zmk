82b4a8943b5413b3da621dfe8cc4721cff602de2
Tongfeng <68142426+fengzixiaopi@users.noreply.github.com>
2022-12-09 00:19:54 -0800

    Update trackpad.c

--- app/boards/arm/nice_nano/trackpad/trackpad.c
+++ app/boards/arm/nice_nano/trackpad/trackpad.c
@@ -16,59 +16,59 @@ const struct device *trackpad = DEVICE_DT_GET(DT_INST(0, cirque_pinnacle));
 
 static void handle_trackpad(const struct device *dev, const struct sensor_trigger *trig) {
     LOG_ERR("handle trackpad invoked");
-    static uint8_t last_pressed = 0;
-    int ret = sensor_sample_fetch(dev);
-    if (ret < 0) {
-        LOG_ERR("fetch: %d", ret);
-        return;
-    }
-    // LOG_ERR("trackpad sensor sample fetched");
-    struct sensor_value dx, dy, btn;
-    ret = sensor_channel_get(dev, SENSOR_CHAN_POS_DX, &dx);
-    if (ret < 0) {
-        LOG_ERR("get dx: %d", ret);
-        return;
-    }
-    // LOG_ERR("trackpad sensor channel get 1");
-    ret = sensor_channel_get(dev, SENSOR_CHAN_POS_DY, &dy);
-    if (ret < 0) {
-        LOG_ERR("get dy: %d", ret);
-        return;
-    }
-    // LOG_ERR("trackpad sensor channel get 2");
-    ret = sensor_channel_get(dev, SENSOR_CHAN_PRESS, &btn);
-    if (ret < 0) {
-        LOG_ERR("get btn: %d", ret);
-        return;
-    }
-    // LOG_ERR("trackpad sensor channel get 3");
-    LOG_ERR("trackpad %d %d %02x", dx.val1, dy.val1, btn.val1);
-    zmk_hid_mouse_movement_set(0, 0);
-    // LOG_ERR("trackpad mouse movement set");
-    zmk_hid_mouse_scroll_set(0, 0);
-    // LOG_ERR("trackpad mouse scroll set");
-    const uint8_t layer = zmk_keymap_highest_layer_active();
-    uint8_t button;
-    static uint8_t last_button = 0;
-    static int8_t scroll_ver_rem = 0, scroll_hor_rem = 0;
-    if (layer == 2) {   // lower
-        const int16_t total_hor = dx.val1 + scroll_hor_rem, total_ver = -(dy.val1 + scroll_ver_rem);
-        scroll_hor_rem = total_hor % SCROLL_DIV_FACTOR;
-        scroll_ver_rem = total_ver % SCROLL_DIV_FACTOR;
-        zmk_hid_mouse_scroll_update(total_hor / SCROLL_DIV_FACTOR, total_ver / SCROLL_DIV_FACTOR);
-        button = RCLK;
-    } else {
-        zmk_hid_mouse_movement_update(CLAMP(dx.val1, INT8_MIN, INT8_MAX), CLAMP(dy.val1, INT8_MIN, INT8_MAX));
-        button = LCLK;
-    }
-    if (!last_pressed && btn.val1) {
-        zmk_hid_mouse_buttons_press(button);
-        last_button = button;
-    } else if (last_pressed && !btn.val1) {
-        zmk_hid_mouse_buttons_release(last_button);
-    }
-    zmk_endpoints_send_mouse_report();
-    last_pressed = btn.val1;
+    // static uint8_t last_pressed = 0;
+    // int ret = sensor_sample_fetch(dev);
+    // if (ret < 0) {
+    //     LOG_ERR("fetch: %d", ret);
+    //     return;
+    // }
+    // // LOG_ERR("trackpad sensor sample fetched");
+    // struct sensor_value dx, dy, btn;
+    // ret = sensor_channel_get(dev, SENSOR_CHAN_POS_DX, &dx);
+    // if (ret < 0) {
+    //     LOG_ERR("get dx: %d", ret);
+    //     return;
+    // }
+    // // LOG_ERR("trackpad sensor channel get 1");
+    // ret = sensor_channel_get(dev, SENSOR_CHAN_POS_DY, &dy);
+    // if (ret < 0) {
+    //     LOG_ERR("get dy: %d", ret);
+    //     return;
+    // }
+    // // LOG_ERR("trackpad sensor channel get 2");
+    // ret = sensor_channel_get(dev, SENSOR_CHAN_PRESS, &btn);
+    // if (ret < 0) {
+    //     LOG_ERR("get btn: %d", ret);
+    //     return;
+    // }
+    // // LOG_ERR("trackpad sensor channel get 3");
+    // LOG_ERR("trackpad %d %d %02x", dx.val1, dy.val1, btn.val1);
+    // zmk_hid_mouse_movement_set(0, 0);
+    // // LOG_ERR("trackpad mouse movement set");
+    // zmk_hid_mouse_scroll_set(0, 0);
+    // // LOG_ERR("trackpad mouse scroll set");
+    // const uint8_t layer = zmk_keymap_highest_layer_active();
+    // uint8_t button;
+    // static uint8_t last_button = 0;
+    // static int8_t scroll_ver_rem = 0, scroll_hor_rem = 0;
+    // if (layer == 2) {   // lower
+    //     const int16_t total_hor = dx.val1 + scroll_hor_rem, total_ver = -(dy.val1 + scroll_ver_rem);
+    //     scroll_hor_rem = total_hor % SCROLL_DIV_FACTOR;
+    //     scroll_ver_rem = total_ver % SCROLL_DIV_FACTOR;
+    //     zmk_hid_mouse_scroll_update(total_hor / SCROLL_DIV_FACTOR, total_ver / SCROLL_DIV_FACTOR);
+    //     button = RCLK;
+    // } else {
+    //     zmk_hid_mouse_movement_update(CLAMP(dx.val1, INT8_MIN, INT8_MAX), CLAMP(dy.val1, INT8_MIN, INT8_MAX));
+    //     button = LCLK;
+    // }
+    // if (!last_pressed && btn.val1) {
+    //     zmk_hid_mouse_buttons_press(button);
+    //     last_button = button;
+    // } else if (last_pressed && !btn.val1) {
+    //     zmk_hid_mouse_buttons_release(last_button);
+    // }
+    // zmk_endpoints_send_mouse_report();
+    // last_pressed = btn.val1;
 }
 
 static int trackpad_init() {
